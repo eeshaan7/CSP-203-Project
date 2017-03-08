fe00ec3d3227be1fad0d57912d4fc6310cffedc2
eeshaan7 <2015csb1011@iitrpr.ac.in>
2017-03-08 11:08:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php $ptitle = "Lost and Found" ?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/Lost and Found/inc/header.php';?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/Lost and Found/inc/menu.php';?>
+<?php require_once $_SERVER["DOCUMENT_ROOT"]. '/Lost and Found/inc/footer.php';?>
